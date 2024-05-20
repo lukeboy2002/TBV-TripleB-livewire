cac3229c42343bc410eb82d54bc9072efd2efcc3
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-05-20 19:39:20 +0200

    start layout
    Main Layout.
    To be continued

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-gray-50 dark:bg-gray-700 min-h-24 hidden md:block border-b border-orange-500/30">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between items-center">
+            <x-application-logo />
+            <div class="flex items-center space-x-20">
+                <x-contact-us />
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,27 +17,37 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-50 dark:bg-gray-800">
         <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
+        <x-header />
+        @livewire('navigation-menu')
+        <div class="min-h-screen">
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            @if (isset($hero))
+                <div class="bg-gray-50 dark:bg-gray-700 shadow">
+                    <div class="w-full mx-auto">
+                        {{ $hero }}
                     </div>
-                </header>
+                </div>
             @endif
 
             <!-- Page Content -->
-            <main>
+            @if (isset($side))
+            <div class="max-w-7xl mx-auto flex flex-wrap py-10 sm:px-6 lg:px-8">
+                <main class="w-full md:w-3/4 flex flex-col px-3">
+                    {{ $slot }}
+                </main>
+                <aside class="w-full md:w-1/4 flex flex-col px-3">
+                    <slot name="side" />
+                </aside>
+            </div>
+            @else
+            <main class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 {{ $slot }}
             </main>
+            @endif
         </div>
-
+        <x-footer />
         @stack('modals')
 
         @livewireScripts
