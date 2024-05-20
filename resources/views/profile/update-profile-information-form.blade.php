20c1f4a934c9584d4a6452c583551c9f035eeb5a
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-05-20 12:40:21 +0200

    Installation and Config
    Installed:
    Statie Permissions
    Statie MediaLibrary
    Eloquent-sluggable
    Avatargenerator
    Flowbite
    
    Changed login from email to username

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->username }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
@@ -42,11 +42,11 @@
                     {{ __('Select A New Photo') }}
                 </x-secondary-button>
 
-                @if ($this->user->profile_photo_path)
-                    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
-                        {{ __('Remove Photo') }}
-                    </x-secondary-button>
-                @endif
+{{--                @if ($this->user->profile_photo_path)--}}
+{{--                    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">--}}
+{{--                        {{ __('Remove Photo') }}--}}
+{{--                    </x-secondary-button>--}}
+{{--                @endif--}}
 
                 <x-input-error for="photo" class="mt-2" />
             </div>
@@ -54,9 +54,9 @@
 
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
-            <x-input-error for="name" class="mt-2" />
+            <x-label for="username" value="{{ __('Username') }}" />
+            <x-input id="username" type="text" class="mt-1 block w-full" wire:model="state.username" required autocomplete="username" />
+            <x-input-error for="username" class="mt-2" />
         </div>
 
         <!-- Email -->
